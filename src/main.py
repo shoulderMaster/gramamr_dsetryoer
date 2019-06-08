4abe0804bf6a60377a764b7e586043e66f4a120d
shoulderMaster <ckdqja0225@gmail.com>
2019-06-08 17:48:51 +0900

    add seperate

---
+++ src/main.py
@@ -0,0 +1,28 @@
+def seperate(p_ch):
+
+    # 초성 리스트. 00 ~ 18
+    CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    # 중성 리스트. 00 ~ 20
+    JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+    # 종성 리스트. 00 ~ 27 + 1(1개 없음)
+    JONGSUNG_LIST = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+    stride_a = len(JUNGSUNG_LIST)*len(JONGSUNG_LIST)
+    stride_b = len(JONGSUNG_LIST)
+    if '가' <= p_ch and p_ch <= '힣' :
+        ch1 = (ord(p_ch) - ord('가'))//stride_a
+        ch2 = ((ord(p_ch) - ord('가')) - stride_a*ch1) // stride_b
+        ch3 = (ord(p_ch) - ord('가')) - stride_a*ch1 - stride_b*ch2
+        return (CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2], JONGSUNG_LIST[ch3])
+    else:
+        return tuple()
+
+def combine(p_char_tuple) :
+    pass
+
+def main() :
+    test = "동해물과 백두산이 마르고 닳도록"
+    for i in test :
+        print(seperate(i))
+
+main()
